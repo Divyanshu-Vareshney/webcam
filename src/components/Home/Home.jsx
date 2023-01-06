d3f742ca0774c26a0f6d72400266d4e05e91d8d7
Divyanshu-Vareshney <varshneydivyanshu2904@gmail.com>
2023-01-06 14:58:28 +0530

    pushing

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,12 @@
+import { WebcamCapture} from '../Webcam/Webcam'
+
+const Home = () => {
+
+    return (
+                    <>
+                        <WebcamCapture/>
+                        <button type="submit" id="login-button" >Proceed</button>
+                    </>
+    )
+}
+export default Home
